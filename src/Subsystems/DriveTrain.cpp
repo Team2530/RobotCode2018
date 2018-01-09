291114b40ce3374744e42796b1635d2456c9c2a3
Celeste Dyrbye-Wright <CelesteDW173@gmail.com>
2018-01-08 20:08:33 -0600

    created drivetrain
    sorry life is hard

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -2,10 +2,7 @@
 #include "../RobotMap.h"
 
 DriveTrain::DriveTrain() : Subsystem("DriveTrainSubsystem") {
-	frontLeftController = new VictorSP(kFrontLeftChannel);
-	rearLeftController = new VictorSP(kRearLeftChannel);
-	frontRightController = new VictorSP(kFrontRightChannel);
-	rearRightController = new VictorSP(kRearRightChannel);
+
 }
 
 void DriveTrain::InitDefaultCommand() {
