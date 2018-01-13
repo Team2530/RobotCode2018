2044b81837248c59ecc955e98c0360f26abe9a1e
Unknown <pwmarshall@gmail.com>
2018-01-13 13:42:00 -0600

    DriveTrain
    added drive train subsystem

---
+++ src/Subsystems/DriveTrain.cpp
@@ -0,0 +1,14 @@
+#include "DriveTrain.h"
+#include "../RobotMap.h"
+
+DriveTrain::DriveTrain() : Subsystem("ExampleSubsystem") {
+
+}
+
+void DriveTrain::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
