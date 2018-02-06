165ba0ec0baea1b46acd5766298ba911f627d37a
Unknown <sirmax124@gmail.com>
2018-02-05 18:38:43 -0600

    Added More Auto Commands

---
+++ src/Commands/StartLeftDeliverLeft.h
@@ -0,0 +1,11 @@
+#ifndef StartLeftDeliverLeft_H
+#define StartLeftDeliverLeft_H
+
+#include <Commands/CommandGroup.h>
+
+class StartLeftDeliverLeft : public CommandGroup {
+public:
+	StartLeftDeliverLeft();
+};
+
+#endif  // StartLeftDeliverLeft_H
---
+++ src/Commands/StartRightDeliverLeft.h
@@ -0,0 +1,11 @@
+#ifndef StartRightDeliverLeft_H
+#define StartRightDeliverLeft_H
+
+#include <Commands/CommandGroup.h>
+
+class StartRightDeliverLeft : public CommandGroup {
+public:
+	StartRightDeliverLeft();
+};
+
+#endif  // StartRightDeliverLeft_H
