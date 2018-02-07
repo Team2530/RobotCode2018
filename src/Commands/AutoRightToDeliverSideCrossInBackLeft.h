fff8080a9f68db76647e4c7f1964e2427330b24f
Celeste Dyrbye-Wright <CelesteDW173@gmail.com>
2018-02-07 15:10:46 -0600

    Did the other deliver cube by crossing in back
    both of those should (SHOULD) be done now. That means all the cross in back should be done since left + left -> deliver side front and right + right ->deliver side front as well.

---
+++ src/Commands/AutoRightToDeliverSideCrossInBackLeft.h
@@ -0,0 +1,24 @@
+#ifndef AutoRightToDeliverSideCrossInBackLeft_H
+#define AutoRightToDeliverSideCrossInBackLeft_H
+
+#include <Commands/CommandGroup.h>
+#include <Robot.h>
+#include <Commands/TurnDegrees.h>
+#include <Commands/GoStraight.h>
+
+class AutoRightToDeliverSideCrossInBackLeft : public CommandGroup {
+private:
+	double distancePastSwitch = 196; //14ft to center, 2ft,4in past rail
+	double distancePastCube = 13;//1ft 1 in
+	double distanceToCenterSwitchFromCube = 28;//4ft 8 inches is all, so middle is 2ft 4 inches
+	double bufferY = 5;//extra room so not up against cubes
+	double bufferX = 5;
+	double distanceBaseLinePastSwitch = distancePastSwitch+distancePastCube+bufferY;
+	double distancePassSwitchLongWay=153 + bufferX;//12 ft 9.5inch + buffer
+	double distanceLineUpSwitch = distancePastCube+distanceToCenterSwitchFromCube + bufferY;
+	double distanceFinalToSwitch = bufferX;
+public:
+	AutoRightToDeliverSideCrossInBackLeft();
+};
+
+#endif  // AutoRightToDeliverSideCrossInBackLeft_H
---
+++ src/DriveDistances.h
@@ -0,0 +1,17 @@
+/*
+ * DriveDistances.h
+ *
+ *  Created on: Feb 6, 2018
+ *      Author: CelesteDW173
+ */
+
+#ifndef SRC_DRIVEDISTANCES_H_
+#define SRC_DRIVEDISTANCES_H_
+
+enum DriveDistances{
+	DISTANCE_BASELINE_PAST_SWITCH
+};
+
+
+
+#endif /* SRC_DRIVEDISTANCES_H_ */
