e07ad3b7831c88627df9ed9f6a76b671bc6197cc
Unknown <mep6981@gmail.com>
2018-02-08 19:52:06 -0600

    Auto stash before merge of "master" and "origin/master"

--- src/Robot.h
+++ src/Robot.h
@@ -14,6 +14,34 @@
 #include <Subsystems/Ramp.h>
 #include <StartPositions.h>
 
+//Auto Commands
+#include "Commands/DoNothing.h"
+
+#include "Commands/AutoLeftToCrossLineLeft.h"
+#include "Commands/AutoLeftToDeliverFrontRight.h"
+#include "Commands/AutoLeftToDeliverFrontLeft.h"
+//#include "Commands/AutoLeftToDeliverSideCrossInFrontLeft.h"
+//#include "Commands/AutoLeftToDeliverSideCrossInFrontRight.h"
+#include "Commands/AutoLeftToDeliverSideCrossInBackRight.h"
+
+#include "Commands/AutoMiddleToCrossLineLeft.h"
+#include "Commands/AutoMiddleToCrossLineRight.h"
+#include "Commands/AutoMiddleToDeliverFrontRight.h"
+#include "Commands/AutoMiddleToDeliverFrontLeft.h"
+#include "Commands/AutoMiddleToDeliverSideCrossInFrontLeft.h"
+#include "Commands/AutoMiddleToDeliverSideCrossInFrontRight.h"
+
+#include "Commands/AutoRightToCrossLineRight.h"
+#include "Commands/AutoRightToDeliverFrontRight.h"
+#include "Commands/AutoRightToDeliverFrontLeft.h"
+//#include "Commands/AutoRightToDeliverSideCrossInFrontRight.h"
+#include "Commands/AutoRightToDeliverSideCrossInFrontLeft.h"
+#include "Commands/AutoRightToDeliverSideCrossInBackLeft.h"
+
+
+#include <SmartDashboard/SendableChooser.h>
+
+
 class Robot : public frc::TimedRobot {
 public:
 
@@ -54,7 +82,7 @@ private:
 	//frc::SendableChooser<frc::Command*> m_chooser;
 	frc::SendableChooser<AutoCommand> AutoChooserLeft;
 	frc::SendableChooser<AutoCommand> AutoChooserRight;
-	frc::SendableChooser<StartPosition*> ChooserPos;
+	frc::SendableChooser<StartPosition> ChooserPos;
 };
 
 #endif /* SRC_ROBOT_H_ */
