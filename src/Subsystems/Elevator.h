8da6bf58b87d19a138ca869f9e780b0f76b51bea
Celeste Dyrbye-Wright <CelesteDW173@gmail.com>
2018-02-10 10:18:53 -0600

    Elevator and AutoDeliverCube
    works IN THEORY

--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -1,10 +1,14 @@
 #ifndef Elevator_H
 #define Elevator_H
-
+#include <WPILib.h>
 #include <Commands/Subsystem.h>
 
 class Elevator : public Subsystem {
 private:
+	static constexpr int ChannelElevator = 0; //placeholder number
+	VictorSP* ElevatorMotor;
+	static constexpr double minPow = 0.1;
+	static constexpr double maxPow = 0.7;
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
 
@@ -12,6 +16,7 @@ public:
 	Elevator();
 	void InitDefaultCommand();
 	void Raise();
+	void RaiseAuto();
 };
 
 #endif  // Elevator_H
