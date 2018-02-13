3a061822edac8679e46cb399acbe10bfa5a79b50
Akea <galatyspero@gmail.com>
2018-02-12 19:17:49 -0600

    Added LeftSideBackup
    I believe I did this right not quite sure.

---
+++ src/Commands/LeftSideBackUp.h
@@ -0,0 +1,11 @@
+#ifndef LeftSideBackUp_H
+#define LeftSideBackUp_H
+
+#include <Commands/CommandGroup.h>
+
+class LeftSideBackUp : public CommandGroup {
+public:
+	LeftSideBackUp();
+};
+
+#endif  // LeftSideBackUp_H
