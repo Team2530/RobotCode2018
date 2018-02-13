b4740197a4181a71ecc0722ed17b9b9e6ffb633e
Unknown <mep6981@gmail.com>
2018-02-12 20:18:34 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Commands/AutoEvadeFrontLeft.h
@@ -0,0 +1,11 @@
+ #ifndef AutoEvadeFrontLeft_H
+#define AutoEvadeFrontLeft_H
+
+#include <Commands/CommandGroup.h>
+
+class AutoEvadeFrontLeft : public CommandGroup {
+public:
+	AutoEvadeFrontLeft();
+};
+
+#endif  // AutoEvadeFrontLeft_H
