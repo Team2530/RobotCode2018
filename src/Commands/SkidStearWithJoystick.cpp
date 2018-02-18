d25974c90019ccd3a558e0d70e2daa8603689520
Unknown <luckymagicmead@gmail.com>
2018-02-17 18:07:23 -0600

    Added stopping to skidstear

--- src/Commands/SkidStearWithJoystick.cpp
+++ src/Commands/SkidStearWithJoystick.cpp
@@ -6,6 +6,7 @@
 SkidStearWithJoystick::SkidStearWithJoystick() {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
+	Requires(Robot::drivetrain.get());
 }
 
 // Called just before this Command runs the first time
@@ -26,7 +27,7 @@ bool SkidStearWithJoystick::IsFinished() {
 
 // Called once after isFinished returns true
 void SkidStearWithJoystick::End() {
-
+	Robot::drivetrain->Stop();
 }
 
 // Called when another command which requires one or more of the same
